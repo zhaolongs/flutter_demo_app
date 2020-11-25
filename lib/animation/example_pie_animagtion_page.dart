bab3e01ea609e563442c47deac7ce1359bace457
zhaolong <928343994@qq.com>
2020-11-25 16:36:59 +0800

    完善一下

--- lib/animation/example_pie_animagtion_page.dart
+++ lib/animation/example_pie_animagtion_page.dart
@@ -10,6 +10,8 @@ import 'package:flutter_test_app/tk/demo6/config/colors.dart';
 /// 可关注公众号：我的大前端生涯   获取最新技术分享
 /// 可关注网易云课堂：https://study.163.com/instructor/1021406098.htm
 /// 可关注博客：https://blog.csdn.net/zl18603543572
+/// 西瓜视频 https://www.ixigua.com/home/3662978423
+/// 知乎 https://www.zhihu.com/people/zhao-long-90-89
 ///
 ///代码清单
 ///程序入口
